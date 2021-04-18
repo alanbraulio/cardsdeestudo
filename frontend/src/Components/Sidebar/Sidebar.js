edb270b483f90c8960b1dce32226db1bd19b6e13
Alan Braulio <alan-braulio@hotmail.com>
2021-04-18 16:57:58 -0300

    Finalizando detalhes

--- frontend/src/Components/Sidebar/Sidebar.js
+++ frontend/src/Components/Sidebar/Sidebar.js
@@ -146,7 +146,7 @@ function Sidebar(props) {
           className={template === 3 ? styles.optionSelected : styles.option}
         >
           <div className={styles.figureCircle}></div>
-          <p>Hexagono</p>
+          <p>Círculo</p>
         </div>
       </>
     );
